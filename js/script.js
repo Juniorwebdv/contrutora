38b8a0a91b33e2a348eae863488859473e32bcde
Juniorwebdv <juniorfernandes154@gmail.com>
2025-01-30 14:20:28 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+
+$('.navbar-brand').fadeOut(2000);
+$('.navbar-brand').fadeIn(2000);
+$('.navbar-brand').fadeOut(2000);
+$('.navbar-brand').fadeIn(2000);
+
+
+
+});
